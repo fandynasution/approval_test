f236306ad5167559d6cbb9b19a83a7038a5bece8
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-07-04 16:14:55 +0800

    asd

--- app/Http/Controllers/EnvController.php
+++ app/Http/Controllers/EnvController.php
@@ -8,17 +8,11 @@ class EnvController extends Controller
 {
     public function index()
     {
-        $envVariables = [
-            'APP_ENV' => env('APP_ENV'),
-            'APP_DEBUG' => env('APP_DEBUG'),
-            'DB_CONNECTION' => env('DB_CONNECTION3'),
-            'DB_HOST' => env('DB_HOST3'),
-            'DB_PORT' => env('DB_PORT3'),
-            'DB_DATABASE' => env('DB_DATABASE3'),
-            'DB_USERNAME' => env('DB_USERNAME3'),
-            // Add more variables as needed
-        ];
-
-        var_dump($envVariables);
+        try {
+            DB::connection('BTID')->getPdo();
+            echo "Connected successfully to database.";
+        } catch (\Exception $e) {
+            echo "Could not connect to the database. Please check your configuration. Error: " . $e->getMessage();
+        }
     }
 }
