045a91c6e9d8fa5ab3a2392c682e42d86018257b
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-01-24 10:45:21 +0700

    AZURE

--- config/services.php
+++ config/services.php
@@ -35,6 +35,12 @@ return [
         'redirect' => env('AZURE_REDIRECT_URI'),
         'tenant' => env('AZURE_TENANT_ID'),
     ],
+    'microsoft' => [
+        'client_id' => env('AZURE_CLIENT_ID'),
+        'client_secret' => env('AZURE_CLIENT_SECRET'),
+        'redirect' => env('AZURE_REDIRECT_URI'),
+        'tenant' => env('AZURE_TENANT_ID'), // Optional if you're using a specific tenant
+    ],
 
 
 ];
