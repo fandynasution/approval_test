7b4f9e47681b928949f4e8f8a2e7a38af2792f62
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-01-30 10:00:59 +0700

    Link Status on body email

--- resources/views/email/cbfupd/send.blade.php
+++ resources/views/email/cbfupd/send.blade.php
@@ -75,7 +75,6 @@
                                             https://checkapprovalstatus.kurakurabali.com/
                                         </a>
                                     </p>
-                                    <br>
                                     <p style="text-align:left;margin-bottom: 15px; color: #000000; font-size: 16px;">
                                         In case you need some clarification, kindly approach : <br>
                                         <a href="mailto:{{ $dataArray['clarify_email'] }}" style="text-decoration: none; color: inherit;">
