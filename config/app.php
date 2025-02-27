a57b02d6beede388bbe245a837e40fca67f9c6a9
Ahmad Ariffandy Nasution <fandy@fandy-ifca.ifca.co.id>
2025-02-27 17:14:18 +0700

    aaa

--- config/app.php
+++ config/app.php
@@ -56,6 +56,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'APP_APPR' => env('APP_APPR', 'https://default.url'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
